7bb6fac6e415d401b97eda6a316a639e589251e1
Tomek <tomek.marchel@o2.pl>
2024-06-17 15:00:05 +0200

    Character controller (move & rotate)

--- Zadanie testowe/Assets/Scripts/Player.cs
+++ Zadanie testowe/Assets/Scripts/Player.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed = 5f;
 
-    // Update is called once per frame
+    private Vector3 inputVector;
+
     void Update()
     {
-        
+        inputVector = new Vector2(0,0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1f;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.forward = moveDirection;
+
     }
 }
